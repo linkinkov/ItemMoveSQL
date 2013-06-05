500f92729767315e597d27bfb3cfbf32783fcb98
ptnkjke <ptnkjke@gmail.com>
2013-06-06 01:01:48 +0400

    autoformat

--- src/itemmovesql/DBUtils.java
+++ src/itemmovesql/DBUtils.java
@@ -11,74 +11,63 @@ public class DBUtils {
 
 	private Main main;
 	private ItemMoveSQLConfig config;
-	
-	DBUtils(Main main,ItemMoveSQLConfig config)
-	{
+
+	DBUtils(Main main, ItemMoveSQLConfig config) {
 		this.main = main;
 		this.config = config;
 	}
-	
 
 	private Logger log = Bukkit.getLogger();
-	
-	public Connection getConenction()
-	{
+
+	public Connection getConenction() {
 		return InitConnection();
 	}
-	
-	public void CreateNeeded()
-	{
+
+	public void CreateNeeded() {
 		Connection connection = null;
 		try {
-		if (config.checkdb)
-		{
-		 connection = 	DriverManager.getConnection(config.address,config.login,config.pass);
-		 log.info("[ItemMoveMSQL] Connected to mysql server, creating database if not exists");
+			if (config.checkdb) {
+				connection = DriverManager.getConnection(config.address,
+						config.login, config.pass);
+				log.info("[ItemMoveMSQL] Connected to mysql server, creating database if not exists");
+				Statement st = connection.createStatement();
+				st.executeUpdate("CREATE DATABASE IF NOT EXISTS "
+						+ config.dbname);
+				st.close();
+				connection.close();
+			}
+			connection = DriverManager.getConnection(config.address
+					+ config.dbname, config.login, config.pass);
 			Statement st = connection.createStatement();
-			st.executeUpdate("CREATE DATABASE IF NOT EXISTS "+config.dbname);
+			st.executeUpdate("CREATE TABLE IF NOT EXISTS itemstorage"
+					+ "("
+					+ "keyint int unsigned not null auto_increment primary key,"
+					+ "playername varchar(255)," + "itemid int,"
+					+ "itemsubid int," + "amount int" + ");");
 			st.close();
-		connection.close();
-		}
-		connection = 	DriverManager.getConnection(config.address+config.dbname,config.login,config.pass);
-		Statement st = connection.createStatement();
-		st.executeUpdate(
-				"CREATE TABLE IF NOT EXISTS itemstorage" +
-				"(" +
-				"keyint int unsigned not null auto_increment primary key," +
-				"playername varchar(255)," +
-				"itemid int," +
-				"itemsubid int," +
-				"amount int" +
-				");"
-			);
-		st.close();
-		 log.info("[ItemMoveMSQL] Connected to mysql server and database");
-		 connection.close();
-		 
+			log.info("[ItemMoveMSQL] Connected to mysql server and database");
+			connection.close();
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	
-	public  Connection InitConnection()
-			{
-			try {
-		Connection connection = null;
-				
 
-					connection = 	DriverManager.getConnection(config.address+config.dbname,config.login,config.pass);
+	public Connection InitConnection() {
+		try {
+			Connection connection = null;
 
-					
-					return connection;
+			connection = DriverManager.getConnection(config.address
+					+ config.dbname, config.login, config.pass);
+
+			return connection;
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
 
-			}
-	
+	}
+
 }
--- src/itemmovesql/ItemMoveSQLConfig.java
+++ src/itemmovesql/ItemMoveSQLConfig.java
@@ -8,37 +8,36 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 public class ItemMoveSQLConfig {
 
-	
 	protected String address = "jdbc:mysql://localhost/";
 	protected String dbname = "itemmovesqldb";
 	protected String login = "root";
 	protected String pass = "password";
 	protected int maxitems = 5;
 	protected boolean checkdb = true;
-	public void load()
-	{
-		FileConfiguration config = YamlConfiguration.loadConfiguration(new File("plugins/ItemMoveSQL/config.yml"));
-		address = config.getString("mysql.address",address);
-		dbname = config.getString("mysql.dbname",dbname);
-		login = config.getString("mysql.login",login);
-		pass = config.getString("mysql.password",pass);
-		checkdb = config.getBoolean("mysql.checkdb",checkdb);
+
+	public void load() {
+		FileConfiguration config = YamlConfiguration
+				.loadConfiguration(new File("plugins/ItemMoveSQL/config.yml"));
+		address = config.getString("mysql.address", address);
+		dbname = config.getString("mysql.dbname", dbname);
+		login = config.getString("mysql.login", login);
+		pass = config.getString("mysql.password", pass);
+		checkdb = config.getBoolean("mysql.checkdb", checkdb);
 		maxitems = config.getInt("items.max", maxitems);
-		
-		
-		config.set("mysql.address",address);
-		config.set("mysql.dbname",dbname);
-		config.set("mysql.login",login);
-		config.set("mysql.password",pass);
-		config.set("mysql.checkdb",checkdb);
+
+		config.set("mysql.address", address);
+		config.set("mysql.dbname", dbname);
+		config.set("mysql.login", login);
+		config.set("mysql.password", pass);
+		config.set("mysql.checkdb", checkdb);
 		config.set("items.max", maxitems);
-		
+
 		try {
 			config.save(new File("plugins/ItemMoveSQL/config.yml"));
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 }
